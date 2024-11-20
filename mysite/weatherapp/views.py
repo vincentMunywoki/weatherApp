3a21357d19499972d2da8ab8d71e3be833980429
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-20 13:58:23 +0300

    views file

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -13,6 +13,21 @@ def index(request):
 
         data = {
             "country_code": str(list_of_data['sys']['country']),
+             "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
         }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/index.html", data)
+       
 
     
