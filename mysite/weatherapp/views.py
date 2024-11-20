4ad6ff7f6cf7c96f577a652756b5dd9b070f56ed
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-20 13:40:38 +0300

    views file

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -1,6 +1,10 @@
+import urllib.request  # help us in sending and accepting requests from our APIs
+import json 
 from django.shortcuts import render
 
 # Create your views here.
 
-# def index():
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
     
