8fdf3128ab3e2416e02670e716b6bdbd15de1bf7
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-20 13:53:40 +0300

    views file

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -9,4 +9,10 @@ def index(request):
         city = request.POST['city']
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=a04cc63d5a4ca8b3e3714bc42616cb41').read()
 
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+        }
+
     
