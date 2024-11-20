18f80dc133cbc23a9bcc610de0ff0841cf12e618
vincent munywoki <vincentmunywoki12@gmail.com>
2024-11-20 12:48:56 +0300

    base-dir

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS =(
+# STATICFILES_DIRS =(
+#     os.path.join(BASE_DIR, 'static')
+# )
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-)
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
